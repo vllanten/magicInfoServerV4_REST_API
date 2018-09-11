66caf2e28670773935e9ad61719d55c7a2f167d5
root <root@f08f2b027eb4>
2018-09-11 21:34:44 +0000

    Removed files managed by codegen

--- javascript-closure-angular/API/Client/HashMap?string,object?.js
+++
@@ -1,7 +0,0 @@
-goog.provide('API.Client.HashMap«string,object»');
-
-/**
- * @record
- */
-API.Client.HashMap«string,object» = function() {}
-
--- javascript-closure-angular/API/Client/Map.js
+++
@@ -1,7 +0,0 @@
-goog.provide('API.Client.Map');
-
-/**
- * @record
- */
-API.Client.Map = function() {}
-
